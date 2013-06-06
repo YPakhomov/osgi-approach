567c9f8809c9bd3d5d16ed1e1bf4fff151218a8f
Yuriy <pakhomovyv@gmail.com>
2013-06-06 16:31:04 +0400

    container + spring dm sample

---
+++ springdm-web-sample/src/main/java/facadeapi/FacadeA.java
@@ -0,0 +1,14 @@
+package facadeapi;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author one
+ */
+public interface FacadeA {
+    public String greet(String name);
+}
